92919c7449a7f8380bbfc97fb4aba00955219c89
user_name <email_id>
2018-04-08 22:39:36 -0400

    Added item listing functionality.

---
+++ openbdg/gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-ruby-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    webserver = require('gulp-webserver');
+
+gulp.task('js', function() {
+  return gulp.src('builds/sassEssentials/js/myscript.js')
+    .pipe(jshint('./.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('sass', function () {
+    return sass('process/sass/style.scss', {
+      sourcemap: true,
+      style: 'expanded'
+      // expanded, nested, compact, compressed
+    })
+    .on('error', function (err) {
+        console.error('Error!', err.message);
+    })
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('glossary/static/'));
+});
+
+// 'builds/sassEssentials/css'
+
+gulp.task('watch', function() {
+  // gulp.watch('builds/sassEssentials/js/**/*', ['js']);
+  gulp.watch(['process/sass/**/*'], ['sass']);
+});
+//
+// gulp.task('webserver', function() {
+//     gulp.src('inventory_explorer/templates/inventory_explorer')
+//         .pipe(webserver({
+//             livereload: true,
+//             open: true
+//         }));
+// });
+
+gulp.task('default', ['watch', 'sass']);
